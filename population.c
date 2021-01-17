380a91bf43c2f98b3e1606433e84638316b258e4
pedrohdemedeiros <39886834+pedrohdemedeiros@users.noreply.github.com>
2021-01-16 21:07:15 -0300

    Add files via upload

---
+++ population.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int pop;
+    int fpop;
+    int ys = 0;
+
+    do
+    {
+        pop = get_int("Initial Popularion: ");
+    }
+    while (pop < 9);
+    // TODO: Prompt for end size
+
+    do
+    {
+        fpop = get_int("Final Population: ");
+    }
+    while (fpop < pop);
+
+    // TODO: Calculate number of years until we reach threshold
+
+    while (pop < fpop)
+    {
+        pop = pop + pop/3 - pop/4;
+        ys++;
+    }
+
+
+
+    // TODO: Print number of years
+
+    printf("Years: %i", ys);
+}
